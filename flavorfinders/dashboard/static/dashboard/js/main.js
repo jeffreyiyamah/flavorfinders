4a5d3e984ccf2ec8dd68cb95d4eef7f80af779f5
jeffreyiyamah <jri8@cornell.edu>
2023-07-28 20:11:33 -0500

    Third Commit

--- flavorfinders/dashboard/static/dashboard/js/main.js
+++ flavorfinders/dashboard/static/dashboard/js/main.js
@@ -1,3 +1,35 @@
+// main.js
+
+// Include the jQuery library
+const script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+// Wait for jQuery to load and then execute your jQuery code
+script.onload = function() {
+    // Your jQuery code goes here
+    $(document).ready(function() {
+      $('#profileTab').click(function() {
+        toggleTab('right');
+      });
+  
+      $('#preferencesTab').click(function() {
+        toggleTab('preferences');
+      });
+    });
+  
+    function toggleTab(tabName) {
+      if (tabName === 'right') {
+        $('.preferences').hide();
+        $('.right').show();
+      } else if (tabName === 'preferences') {
+        $('.right').hide();
+        $('.preferences').show();
+      }
+    }
+  };
+
 //Adding the Map and its style to the map
 mapboxgl.accessToken = 'pk.eyJ1IjoiamVmZnJleWl5YW1haCIsImEiOiJjbGlveTZjaHkwaWN5M2NwYXpvajJkaTg1In0.koMqCA4ePB63Mbx1umzdlA';
 const map = new mapboxgl.Map({
